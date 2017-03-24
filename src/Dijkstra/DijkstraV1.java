352b79d8e8fa24ab0eab18170f3374587c4a9cfb
KiiroHanabi <wissam.azzi@outlook.fr>
2017-03-24 17:32:25 +0100

    Changement de nom de dossier

--- src/Dijkstra/DijkstraV1.java
+++ src/Dijkstra/DijkstraV1.java
@@ -1,15 +1,60 @@
-package Dijkstra;
+package dijkstra;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import listeAdjacence.Arc;
+import listeAdjacence.Graphe;
+import listeAdjacence.Noeud;
 
 public class DijkstraV1 {
 
-	public DijkstraV1(int nbSommets, int source){
+	private Graphe g;
+	private HashMap<Integer, PlusCourtChemin> ppc;
+	private ArrayList<Noeud> nonMarques;
+	private int[] p;
+	private double[] d;
+	private int s;
+	
+	public DijkstraV1(Graphe graphe, int source){	
 
-		int p[] = new int[nbSommets];
-		int d[] = new int[nbSommets];
-
-		for(int i=0; i< nbSommets; i++){
-			p[i] = source;
-			//d[i] =
+		if(graphe.getNoeuds().get(s) != null)
+		{
+			g = graphe;
+			ppc = new HashMap<Integer, PlusCourtChemin>();
+			nonMarques = new ArrayList<Noeud>();
+			p = new int[g.getNoeuds().size()];
+			d = new double[g.getNoeuds().size()];
+			s = source;
+			init();
+			loop();
 		}
+		else 
+		{
+			System.err.println("Le noeud source n'est pas présent dans le graphe.");
+		}
+	}
+	
+	public void init()
+	{
+		for(int i=0; i< g.getNoeuds().size(); i++){
+			p[i] = s;
+			d[i] = cout(s,g.getNoeuds().get(i).getId());
+			nonMarques.add(g.getNoeuds().get(i));
+		}
+		nonMarques.remove(g.getNoeuds().get(s));
+	}
+	
+	public void loop()
+	{
+		//TODO reste de l'algo de Dijkstra ici !
+	}
+	
+	public double cout(int source, int fin)
+	{
+		if(source == fin) return 0;
+		Arc arc = g.getNoeuds().get(source).getArcByTerm(g.getNoeuds().get(fin));
+		if(arc == null) return Double.POSITIVE_INFINITY;
+		else return arc.getPoids();
 	}
 }
--- src/Dijkstra/PlusCourtChemin.java
+++ src/Dijkstra/PlusCourtChemin.java
@@ -1,5 +1,34 @@
-package Dijkstra;
+package dijkstra;
+
+import listeAdjacence.Noeud;
 
 public class PlusCourtChemin {
-
+	
+	private double coutPPC;
+	private Noeud perePPC;
+	private boolean pereMarque;
+	
+	public double getCoutPPC() {
+		return coutPPC;
+	}
+	
+	public void setCoutPPC(double coutPPC) {
+		this.coutPPC = coutPPC;
+	}
+	
+	public Noeud getPerePPC() {
+		return perePPC;
+	}
+	
+	public void setPerePPC(Noeud perePPC) {
+		this.perePPC = perePPC;
+	}
+	
+	public boolean isPereMarque() {
+		return pereMarque;
+	}
+	
+	public void setPereMarque(boolean pereMarque) {
+		this.pereMarque = pereMarque;
+	}
 }
